945404803856a8fbfb5aa48f621edfa70ca25602
Iamyangbo <807490539@qq.com>
2018-08-12 15:21:29 +0800

    V3.0

--- currency_converter_v2.0.py
+++ currency_converter_v2.0.py
@@ -7,10 +7,8 @@
 """
 # 汇率
 USD_VS_RMB = 6.77
-
 # 带单位的货币输入
 currency_str_value = input('请输入带单位的货币金额：')
-
 # 获取货币单位
 unit = currency_str_value[-3:]
 value_str = currency_str_value[:-3]
@@ -26,6 +24,13 @@ elif unit == 'USD':
     print('人民币金额：', rmb_value)
 else:
     print('暂时不支持兑换')
+
+
+
+
+
+
+
 
 # change_str_way = input('请选择您要兑换的币种： 1.人民币(RMB)    2.美元(USD)')
 # change_way = eval(change_str_way)
