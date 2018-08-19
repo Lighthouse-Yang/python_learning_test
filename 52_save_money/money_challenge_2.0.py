6af92b2f58d01d6a12d3923c9163ad5361fd3489
Iamyangbo <807490539@qq.com>
2018-08-19 20:14:29 +0800

    2.0

--- 52_save_money/money_challenge_2.0.py
+++ 52_save_money/money_challenge_2.0.py
@@ -1,10 +1,12 @@
 """
     作者：Yang
     功能：52周存钱挑战
-    新增功能：
+    新增功能：需要记录每周存款金额.
+    方法：利用math库、列表功能
     版本：2.0
     日期：19/08/2018
 """
+import math
 
 
 def main():
@@ -17,10 +19,16 @@ def main():
     total_week = 52        # 总周数
     saving = 0             # 账户累计
 
+    money_list = []
+
     while i <= total_week:
         # 存钱操作
         # saving = money_per_week + saving    两句相等
-        saving += money_per_week
+        # saving += money_per_week
+        # append()函数功能：将元素添加到列表末尾.
+        money_list.append(money_per_week)
+
+        saving = math.fsum(money_list)
         # 输出信息
         print('第{}周，存入{}元，账号累计{}元'.format(i, money_per_week, saving))
         # money_per_week = money_per_week + increase_money
