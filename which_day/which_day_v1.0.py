dbc4a1ba630c183dc2f3a94a867209fa20906341
Iamyangbo <807490539@qq.com>
2018-08-22 00:46:28 +0800

    end1.0

--- which_day/which_day_v1.0.py
+++ which_day/which_day_v1.0.py
@@ -15,28 +15,28 @@ def main():
     input_date_str = input('请输入日期(yyyy/mm/dd):')
     input_date = datetime.datetime.strptime(input_date_str, '%Y/%m/%d')
     year = input_date.year
-    print(type(year))
     month = input_date.month
-    print(month)
     day = input_date.day
     ping_year = ('31', '28', '31','30', '31', '30', '31', '31', '30', '31', '30', '31')
     run_year = ('31', '29', '31','30', '31', '30', '31', '31', '30', '31', '30', '31')
     # print(ping_year)
     # print(type(ping_year))       # 元组类型
     # print(type(ping_year[2]))    # 字符串类型
-    print(int(run_year[month]))
+
     i = 1
     s = 0
     if year % 400 == 0:
         # 能整除400则为闰年
 
         while i < month:
-            s = s + 1
-        days = s +day
+            s = s +int(run_year[i - 1])
+            i = i + 1
+        days = s + day
         print('闰年', days)
     else:
         while i < month:
             s = s + int(ping_year[i - 1])
+            i = i + 1
         days = s + day
         print('平年', days)
 
