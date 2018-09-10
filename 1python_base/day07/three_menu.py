987526cd36535128f5b2563b3cad741b31968420
Iamyangbo <807490539@qq.com>
2018-09-10 14:03:41 +0800

    dayo6

--- 1python_base/day07/three_menu.py
+++ 1python_base/day07/three_menu.py
@@ -73,21 +73,42 @@ menu = {
     }
 }
 # 方法1：
-while True:
+back_flag = False
+exit_flag = False
+while not back_flag:
     for key in menu:
         print(key)
     choice = input("1>>:").strip()
+    if choice == 'q':
+        exit_flag = True
     if choice in menu:
-        while True: #程序停在第二层.
+        while not back_flag and not exit_flag: #程序停在第二层.
             for key2 in menu[choice]:
                 print(key2)
             choice2 = input("2>>:").strip()
+            if choice2 == 'b':
+                back_flag = True
+            if choice2 == 'q':
+                exit_flag = True
             if choice2 in menu[choice]:
                 for key3 in menu[choice][choice2]:
                     print(key3)
                 choice3 = input("3>>:").strip()
+                if choice3 == 'b':
+                    back_flag = True
                 if choice3 in menu[choice][choice2]:
-                    while True:
+                    while not back_flag:
                         for key4 in menu[choice][choice2][choice3]:
                             print(key4)
                         choice4 = input("4>>:").strip()
+                        print('last level')
+                        if choice4 == 'b':
+                            back_flag = True
+                        if choice4 == 'q':
+                            back_flag = True
+                    else:
+                        back_flag = False
+        else:
+            back_flag = False
+else:
+    back_flag = False
